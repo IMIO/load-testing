347dcb2210e71ea652f91fd65381426237ec6de3
Jean-François Roche <jfroche@affinitic.be>
2016-08-24 09:58:30 +0200

    Add docker network and add consul machine in swarm

--- create-cluster.sh
+++ create-cluster.sh
@@ -17,8 +17,10 @@ function setup_docker_local_env() {
 main() {
 	local args=(
 		- "Init: Create consul machine" create_consul_machine
-		- "Init: Create consul service" create_consul_service
+		- "Init: Create consul services" create_consul_services
 		- "Site: Create site machine"		create_site_machine
+                - "Init: Create docker network"         create_docker_network
+                - "Init: Add consul machine in cluster" add_consul_machine_to_swarm
 		--sequential
                 - "Site: Create registrator service"  create_registrator_service
                 - "Site: Create remote directories"	create_remote_dirs
@@ -42,8 +44,11 @@ main() {
                 --before  "Site: Start site"
 
 		- "JMeter slave: Create jmeter slave machine"		create_jmeter_slave_machine jmeter-slave-1
+                --require "Init: Create consul services"
+                --before "JMeter slave: Create jmeter slave machine"
+
                 - "JMeter slave: Sync jmeter data"                    sync_jmeter_data jmeter-slave-1
-                - "JMeter slave: Create jmeter slave container"       create_jmeter_slave_container
+                - "JMeter slave: Create jmeter slave container"       create_jmeter_slave_container jmeter-slave-1
                 --require "JMeter slave: Create jmeter slave machine"
                 --before "JMeter slave: Sync jmeter data"
                 --before "JMeter slave: Create jmeter slave container"
